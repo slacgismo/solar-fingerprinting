9194aaefd5aea87e7bb799eb1ddd6f672c130017
Bennet Meyers <bennetm@stanford.edu>
2020-03-20 18:06:51 -0700

    initial algorithm

---
+++ solarfingerprinting/fingerprint.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+''' Fingerprinting algorithm modules
+This module contains the fingerprinting algorithm for daily PV power signals
+'''
+
+import numpy as np
+from scipy import optimize
+from inspect import signature
+from solarfingerprinting.pulses import gaussian, gpow, glin, gquad, gatan, g2
+
+FUNCTIONS = {
+    'gauss': gaussian,
+    'gauss_power': gpow,
+    'gauss_linear': glin,
+    'gauss_quad': gquad,
+    'gauss_atan2': gatan,
+    'gaus_lin_mixture': g2
+}
+
+def fingerprint(data, function='gauss_quad', return_fit=True, return_rmse=True,
+                residuals=None, reweight=False):
+    num_meas_per_hour = len(data) / 24
+    x = np.arange(0, 24, 1. / num_meas_per_hour)
+    f = FUNCTIONS[function]
+    n_args = len(signature(f).parameters) - 1
+    init = np.zeros(n_args)
+    if residuals is None:
+        residuals = np.ones_like(data)
+    try:
+        optimal_params, _ = optimize.curve_fit(f, x[1:],
+                                               data[1:],
+                                               p0=init,
+                                               sigma=residuals[1:],
+                                               maxfev=100000)
+    except RuntimeError:
+        optimal_params = init
+    fit = f(x, *optimal_params)
+    residual = data - fit
+    rmse = np.linalg.norm(residual) / np.sqrt(len(data))
+    if reweight:
+        optimal_params, fit, rmse = fingerprint(data, function=function,
+                                                residuals=residual + 1e-3)
+    result = [optimal_params]
+    if return_fit == True:
+        result.append(fit)
+    if return_rmse == True:
+        result.append(rmse)
+    return optimal_params, fit, rmse
---
+++ solarfingerprinting/pulses.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+''' Pulses module
+This module contains functions which parameterized functions which define
+pulses of various shapes. The fingerprinting algorithm utilizes the Gaussian
+pulse modified by a quadratic, or "gquad". All the functions translate and
+scale the parameter inputs. The point of this is so that
+scipy.optimize.curve_fit can find optimal parameter values for typical normalized
+PV power signals.
+'''
+
+import numpy as np
+from scipy.special import erfc
+
+def gaussian(x, loc, scale, beta, amplitude):
+    x = x.astype(np.float)
+    a_p = np.exp(amplitude)
+    loc_p = loc + 12
+    scale_p = scale + 4
+    beta_p =  beta + 1
+    return a_p * np.exp(-(np.abs(x - loc_p) / scale_p) ** beta_p)
+
+def gpow(x, loc, scale, beta, amplitude, k):
+    x = x.astype(np.float)
+    a_p = np.exp(amplitude)
+    loc_p = loc + 12
+    scale_p = scale + 4
+    beta_p =  beta + 1
+    k_p = k + 1
+    return a_p * (x ** (k_p - 1)) * np.exp(-(np.abs(x - loc_p) / scale_p) ** beta_p)
+
+def glin(x, loc, scale, beta, m, b):
+    x = x.astype(np.float)
+    loc_p = loc + 12
+    scale_p = scale + 4
+    beta_p =  beta + 1
+    b_p = b + 1
+    return (m * x + b_p) * np.exp(-(np.abs(x - loc_p) / scale_p) ** beta_p)
+
+def gquad(x, loc, scale, beta, a, b, c):
+    x = x.astype(np.float)
+    loc_p = loc + 12
+    scale_p = scale + 4
+    beta_p =  3 * beta + 6
+    # The following three parameters control the quadratic functions. The "raw"
+    # parameters are highly correlated, so we sequentially orthogonalize the
+    # dimensions. The magnitudes are selected based on values that successfully
+    # orthagonalized a randomized set of daily signals.
+    a_p = a / 10
+    b_p = b / 10 - 2.4 * a
+    c_p = c + 14 * a - 1.2 * b
+    t1 = np.exp(a_p * x ** 2 + b_p * x + c_p)
+    t2 = np.exp(-(np.abs(x - loc_p) / scale_p) ** beta_p)
+    return t1 * t2
+
+def gquad2(x, alpha, beta, a, b, c, k):
+    x_tilde = x - len(x) / 2
+    expr = alpha * x_tilde ** beta + a * x_tilde ** 2 + b * x_tilde + c
+    f = k * np.exp(expr)
+    return f
+
+def gatan(x, loc, scale, beta, a, s, u, v):
+    x = x.astype(np.float)
+    loc_p = loc + 12
+    scale_p = scale + 4
+    beta_p =  beta + 1
+    a_p = a
+    s_p = s + 1
+    u_p = u # 5 * u + 145
+    v_p = v + 1
+    t1 = (a_p * np.arctan(x * s_p - u_p) + v_p)
+    t2 = np.exp(-(np.abs(x - loc_p) / scale_p) ** beta_p)
+    return t1 * t2
+
+def g2(x, loc1, scale1, beta1, m1, b1, loc2, scale2, beta2, m2, b2):
+    p1 = glin(x, loc1, scale1, beta1, m1, b1)
+    p2 = glin(x, loc2, scale2, beta2, m2, b2)
+    return p1 + p2
+
+def emg(x, amplitude, mu, sigma, lmbda):
+    p1 = lmbda / 2
+    p2 = np.exp(p1 * (2 * mu + lmbda * sigma ** 2 - 2 * x))
+    p3 = erfc((mu + lmbda * sigma ** 2 - x) / (np.sqrt(2) * sigma))
+    return amplitude * p2 * p3
