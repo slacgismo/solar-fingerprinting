da18c9b1aa92e166fa0ff9c484b0a4e5757fdee0
Bennet Meyers <bennetm@stanford.edu>
2020-04-09 18:44:10 -0700

    better error handling and maximum value normalization

--- solarfingerprinting/fingerprint.py
+++ solarfingerprinting/fingerprint.py
@@ -20,6 +20,7 @@ FUNCTIONS = {
 
 def fingerprint(data, function='gauss_quad', return_fit=True, return_rmse=True,
                 residuals=None, reweight=False, normalize=True):
+    max_val = np.max(data)
     num_meas_per_hour = len(data) / 24
     x = np.arange(0, 24, 1. / num_meas_per_hour)
     f = FUNCTIONS[function]
@@ -29,21 +30,24 @@ def fingerprint(data, function='gauss_quad', return_fit=True, return_rmse=True,
         residuals = np.ones_like(data)
     try:
         optimal_params, _ = optimize.curve_fit(f, x[1:],
-                                               data[1:],
+                                               data[1:] / max_val,
                                                p0=init,
                                                sigma=residuals[1:],
                                                maxfev=100000)
     except RuntimeError:
-        optimal_params = init
-    fit = f(x, *optimal_params)
-    residual = data - fit
-    rmse = np.linalg.norm(residual) / np.sqrt(len(data))
-    if reweight:
-        optimal_params, fit, rmse = fingerprint(data, function=function,
-                                                residuals=residual + 1e-3,
-                                                normalize=False)
-    if normalize and function == 'gauss_quad':
-        optimal_params = forward_transform(optimal_params)
+        optimal_params = None
+        fit = None
+        rmse = None
+    else:
+        fit = f(x, *optimal_params) * max_val
+        residual = data - fit
+        rmse = np.linalg.norm(residual) / np.sqrt(len(data))
+        if reweight:
+            optimal_params, fit, rmse = fingerprint(data, function=function,
+                                                    residuals=residual + 1e-3,
+                                                    normalize=False)
+        if normalize and function == 'gauss_quad':
+            optimal_params = forward_transform(optimal_params)
     result = [optimal_params]
     if return_fit == True:
         result.append(fit)
