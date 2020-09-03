f014106bf6af43eb34032b58ee8842355915c443
Bennet Meyers <bennetm@stanford.edu>
2020-09-03 16:21:28 -0700

    shortcut for cluster analysis class

--- solarfingerprinting/__init__.py
+++ solarfingerprinting/__init__.py
@@ -1,3 +1,4 @@
 from solarfingerprinting.double_dictionary import make_dictionaries
 from solarfingerprinting.preprocessing import batch_process
 from solarfingerprinting.fit_basis import estimate_parameters
+from solarfingerprinting.cluster import ClusterAnalysis
